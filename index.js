98d6a93fc83eb6c1bbb6c1bbf3f98c9cd6f7b452
taylorsieling <taylorasieling@gmail.com>
2021-03-01 13:30:48 -0600

    commit tree b34301e3447fce039c7230c1626853ffb727c45f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+document.querySelector('main#main').remove()
+
+let h1 = document.createElement('h1')
+
+
+document.body.appendChild(h1) 
+
+let newHeader = document.querySelector('h1')
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "Taylor is the champion"
